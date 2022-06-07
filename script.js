0c1f3575600051e39158ae1446ec3d8e367213a3
Dharshan Bellan <dbellan1291@gmail.com>
2022-06-06 18:01:31 -0700

    Create basic functions and buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let operate = function(opp, a, b) {
+    switch (opp) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
